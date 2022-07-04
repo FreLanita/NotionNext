c7c542bc653f137bab90ec9c4c694c910b7cc625
FreLanita <chenshiqi422941259@163.com>
2022-07-04 08:12:20 +0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: '奇了个七', // 作者
   BIO: '保持好奇', // 作者简介
-  LINK: 'https://', // 网站地址
+  LINK: 'https://frelanita.space', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
